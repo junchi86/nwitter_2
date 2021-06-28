cc079bfc030e1553aa3bdbd17511fe8f00a6f203
canine89 <phk707kr@gmail.com>
2021-06-28 17:31:58 +0900

    7장 액션 02까지 실습 완료

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,67 +1,30 @@
-import { authService } from "fbase";
-import { useState } from "react";
+import { authService, firebaseInstance } from "fbase";
+import AuthForm from "components/AuthForm";
 
 const Auth = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [newAccount, setNewAccount] = useState(true);
-
-  const onChange = (event) => {
+  const onSocialClick = async (event) => {
     const {
-      target: { name, value },
+      target: { name },
     } = event;
-    if (name === "email") {
-      setEmail(value);
-    } else if (name === "password") {
-      setPassword(value);
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
     }
+    const data = await authService.signInWithPopup(provider);
   };
-
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      let data;
-
-      if (newAccount) {
-        // create newAccount
-        data = await authService.createUserWithEmailAndPassword(
-          email,
-          password
-        );
-      } else {
-        // log in
-        data = await authService.signInWithEmailAndPassword(email, password);
-      }
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
-  }; 
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          name="email"
-          type="email"
-          placeholder="Email"
-          required
-          value={email}
-          onChange={onChange}
-        />
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          required
-          value={password}
-          onChange={onChange}
-        />
-        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
-      </form>
+      <AuthForm />
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button onClick={onSocialClick} name="google">
+          Continue with Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Continue with Github
+        </button>
       </div>
     </div>
   );
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,35 @@
-const Home = () => <span>Home</span>;
+import { dbService } from "fbase";
+import { useEffect, useState } from "react";
+import Nweet from "components/Nweet";
+import NweetFactory from "components/NweetFactory";
+
+const Home = ({ userObj }) => {
+  const [nweets, setNweets] = useState([]);
+
+  useEffect(() => {
+    dbService.collection("nweets").onSnapshot((snapshot) => {
+      const newArray = snapshot.docs.map((document) => ({
+        id: document.id,
+        ...document.data(),
+      }));
+      setNweets(newArray);
+    });
+  }, []);
+
+  return (
+    <>
+      <NweetFactory userObj={userObj} />
+      <div>
+        {nweets.map((nweet) => (
+          <Nweet
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.uid}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Home;
